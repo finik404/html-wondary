cac33b616c3a154febf706b920b3e68ec20cc7b2
finik404 <finikdigi@gmail.com>
2025-04-02 11:51:41 +0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,278 @@
+// Основной модуль
+import gulp from "gulp";
+// node-modules
+import * as nodePath from "path";
+// Все Плагины
+import notify from "gulp-notify"; // Сообщения (подсказки)
+import browserSync from "browser-sync"; // Локальный сервер
+import newer from "gulp-newer"; // Проверка обновления
+import ifPlugin from "gulp-if"; // Условное ветвление
+import fileinclude from "gulp-file-include"; // Соединение файлов в один
+import webpHtml from "gulp-webp-html-nosvg"; // Вставка webp в html
+import versionNumber from "gulp-version-number"; //
+import {deleteSync} from "del"; // Удаление файлов
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from "gulp-rename"; // Переименование файлов
+import cleanCss from "gulp-clean-css"; // Сжатие CSS файла
+// import webpcss from "gulp-webpcss"; // Вывод WEBP изображений
+import autoprefixer from "gulp-autoprefixer"; // Добавление вендорных префиксов
+import groupCssMediaQueries from "gulp-group-css-media-queries"; // Группировка медиа запросов
+import webp from "gulp-webp"; // Создаем webp
+// import imagemin from "gulp-imagemin"; // Минифицируем картинки
+import fs from "fs"; // Для шрифтов1
+// import ttf2woff2 from "gulp-ttf2woff2"; // Преобразование ttf в woff
+import gulpSvgSprite from "gulp-svg-sprite"; // Создание спрайтов svg
+import webpack from "webpack-stream"; // Использование ES6 синтаксиса
+
+const sass = gulpSass(dartSass); // Преобразование sass в css
+
+// Главные директории
+const rootFolder = nodePath.basename(nodePath.resolve());
+const buildFolder = `./dist`;
+const srcFolder = `./src`;
+
+// Пути
+const path = {
+    build: {
+        html: `${buildFolder}/`,
+        css: `${buildFolder}/css/`,
+        js: `${buildFolder}/js/`,
+        img: `${buildFolder}/img/`,
+        fonts: `${buildFolder}/fonts/`,
+        files: `${buildFolder}/`,
+    }, src: {
+        html: `${srcFolder}/*.html`,
+        scss: `${srcFolder}/scss/style.scss`,
+        js: `${srcFolder}/js/script.js`,
+        img: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svgIcons: `${srcFolder}/img/**/*.svg`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        files: `${srcFolder}/files/**/*.*`,
+    }, watch: {
+        html: `${srcFolder}/**/*.html`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        js: `${srcFolder}/js/**/*.js`,
+        img: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg,ico}`,
+        svgIcons: `${srcFolder}/img/**/*.svg`,
+        fonts: `${srcFolder}/fonts/**/*.*`,
+        files: `${srcFolder}/files/**/*.*`,
+    }, clean: buildFolder, buildFolder: buildFolder, srcFolder: srcFolder, rootFolder: rootFolder,
+};
+
+// Передаем значения в глобальную переменную
+global.app = {
+    isBuild: process.argv.includes("--build"), isDev: !process.argv.includes("--build"),
+};
+
+// Функция для обновления изменений в браузере
+const server = () => {
+    browserSync.init({
+        server: {
+            baseDir: `${path.build.html}`,
+        }, notify: false, port: 3000, open: false,
+    });
+};
+
+// Удаление папки dist
+const reset = (done) => {
+    deleteSync(path.clean);
+    done();
+};
+
+// Функция копирования из папки files
+const copy = (done) => {
+    gulp.src(path.src.files)
+        .pipe(gulp.dest(path.build.files))
+        .on('end', done);
+};
+
+// Функция преобразования html
+const html = () => {
+    return gulp
+        .src(path.src.html)
+        .pipe(fileinclude())
+        .pipe(ifPlugin(app.isBuild, versionNumber({
+            value: "%DT%", append: {
+                key: "_v", cover: 0, to: ["css", "js"],
+            },
+        })))
+        .pipe(gulp.dest(path.build.html))
+        .pipe(browserSync.stream());
+};
+
+// Функция преобразования scss
+const scss = () => {
+    return gulp
+        .src(path.src.scss, {sourcemaps: true})
+        .pipe(sass({
+            outputStyle: "expanded",
+        }).on('error', sass.logError))
+        .pipe(groupCssMediaQueries())
+        // .pipe(ifPlugin(app.isBuild, webpcss({
+        //     webpClass: ".webp", noWebpClass: ".no-webp",
+        // })))
+        .pipe(autoprefixer({
+            grid: true, overrideBrowserslist: ["last 3 versions"], cascade: true,
+        }))
+        .pipe(rename("style.css"))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(cleanCss())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(browserSync.stream())
+};
+
+// Функция преобразования js
+const js = () => {
+    return gulp
+        .src(path.src.js, {sourcemaps: true})
+        .pipe(webpack({
+            mode: 'development', output: {
+                filename: 'script.js'
+            }
+        }))
+        .pipe(gulp.dest(path.build.js))
+        .pipe(browserSync.stream());
+};
+
+// Функция преобразования картинок
+const img = () => {
+    return (gulp
+        .src(path.src.img)
+        .pipe(ifPlugin(app.isBuild, newer(path.build.img)))
+        .pipe(ifPlugin(app.isBuild, webp()))
+        .pipe(ifPlugin(app.isBuild, gulp.dest(path.build.img)))
+        .pipe(gulp.src(path.src.img))
+        .pipe(newer(path.build.img))
+        // .pipe(imagemin({
+        //     progressive: true, svgoPlugins: [{removeViewBox: false}], interlaced: true, optimiazationLevel: 3, // 0 to 7
+        // }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(gulp.src(path.src.svg))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(browserSync.stream()));
+};
+
+// Функции преобразования шрифтов
+const ttf2woff = () => {
+    // Ищем файлы шрифтов .ttf
+    return (gulp
+        .src(`${path.srcFolder}/fonts/*.ttf`, {})
+        // Выгружвем в папку с результтом
+        .pipe(gulp.dest(`${path.build.fonts}`))
+        // Ищем файлы шрифтов .ttf
+        .pipe(gulp.src(`${path.srcFolder}/fonts/*.ttf`, {}))
+        // Конвертация в .woff2
+        // .pipe(ttf2woff2())
+        // Выгружвем в папку с результтом
+        .pipe(gulp.dest(`${path.build.fonts}`)));
+};
+const fontsStyle = () => {
+    // Файл стилей подключения шрифтов
+    let fontsFile = `${path.srcFolder}/scss/assets/fonts.scss`;
+    // Проверяем существуют ли файлы шрифтов
+    fs.readdir(path.build.fonts, function (err, fontsFiles) {
+        if (fontsFiles) {
+            // Проверяем пуст ли файл
+            let file_content = fs.readFileSync(fontsFile);
+            if (file_content == "") {
+                // Если файла нет, создаем его
+                createFontStyles(fontsFile, fontsFiles);
+            } else {
+                // Удаляем файл и созлаем новыми настройками
+                fs.truncate(fontsFile, 0, function () {
+                    console.log("done");
+                });
+                createFontStyles(fontsFile, fontsFiles);
+            }
+        }
+    });
+    return gulp.src(`${path.srcFolder}`);
+
+    function cb() {
+    }
+
+    function createFontStyles(fontsFile, fontsFiles) {
+        fs.writeFile(fontsFile, "", cb);
+        let newFileOnly;
+        for (var i = 0; i < fontsFiles.length; i++) {
+            // Записываем подключения шрифтов в файл стилей
+            let fontFileName = fontsFiles[i].split(".")[0];
+            if (newFileOnly !== fontFileName) {
+                let fontName = fontFileName.split("-")[0] ? fontFileName.split("-")[0] : fontFileName;
+                let fontWeight = fontFileName.split("-")[1] ? fontFileName.split("-")[0] : fontFileName;
+                if (fontWeight.toLowerCase() === "thin") {
+                    fontWeight = 100;
+                } else if (fontWeight.toLowerCase() === "extralight") {
+                    fontWeight = 200;
+                } else if (fontWeight.toLowerCase() === "light") {
+                    fontWeight = 300;
+                } else if (fontWeight.toLowerCase() === "medium") {
+                    fontWeight = 500;
+                } else if (fontWeight.toLowerCase() === "semibold") {
+                    fontWeight = 600;
+                } else if (fontWeight.toLowerCase() === "bold") {
+                    fontWeight = 700;
+                } else if (fontWeight.toLowerCase() === "extrabold" || fontWeight.toLowerCase() === "heavy") {
+                    fontWeight = 800;
+                } else if (fontWeight.toLowerCase() === "black") {
+                    fontWeight = 900;
+                } else {
+                    fontWeight = 400;
+                }
+                fs.appendFile(fontsFile, `@font-face{\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`, cb);
+                newFileOnly = fontFileName;
+            }
+        }
+    }
+};
+
+// Функция создания svg-спрайтов
+const svgSprite = () => {
+    return gulp
+        .src(path.src.svgIcons)
+        .pipe(gulpSvgSprite({
+            mode: {
+                stack: {
+                    sprite: `../icons/icons.svg`,
+                },
+            }, shape: {
+                transform: [{
+                    svgo: {
+                        plugins: [{
+                            removeAttrs: {
+                                attrs: ["fill", "stroke"],
+                            },
+                        },],
+                    },
+                },],
+            },
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(browserSync.stream());
+};
+
+// Наблюдатель за изменениями в файлах
+function watcher() {
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.img, img);
+    gulp.watch(path.watch.svgIcons, svgSprite);
+    gulp.watch(path.watch.fonts, ttf2woff);
+}
+
+// Основные задачи
+const mainTasks = gulp.series(ttf2woff, gulp.parallel(copy, html, scss, js, img, svgSprite));
+
+// Построение сценариев выполнения задач
+const start = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const fonts = fontsStyle
+
+// Экспорт сценариев
+export {fonts};
+
+// Выполлнение сценария по умолчанию
+gulp.task("default", start);
