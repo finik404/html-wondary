22f64e6a1e6c649503945af7d9ad3968e37ecd28
finik404 <finikdigi@gmail.com>
2025-04-04 18:01:50 +0500

    auto check lang pt1

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const customSelect = document.querySelector(".select");
 const trigger = customSelect.querySelector(".trigger");
 const triggerContent = customSelect.querySelector(".trigger_content");
 const options = customSelect.querySelectorAll(".option");
+const STORAGE_KEY = "selected_lang";
 
 // Open select
 trigger.addEventListener("click", () => {
@@ -20,7 +21,7 @@ options.forEach((option) => {
 
     // Save to localStorage
     const lang = option.querySelector("img").getAttribute("alt");
-    if (lang) localStorage.setItem("selected_lang", lang);
+    if (lang) localStorage.setItem(STORAGE_KEY, lang);
   });
 });
 
@@ -33,19 +34,58 @@ document.addEventListener("click", (e) => {
 
 // On ready
 window.addEventListener("DOMContentLoaded", () => {
-  const savedLang = localStorage.getItem("selected_lang");
-  if (!savedLang) return;
+  const savedLang = localStorage.getItem(STORAGE_KEY);
 
+  let langToSelect = savedLang;
+
+  // Set lang from browser lang
+  if (langToSelect) {
+    const cyrillicLanguages = [
+      "ru", // Русский
+      "be", // Белорусский
+      "uk", // Украинский
+      "kk", // Казахский
+      "ky", // Киргизский
+      "uz", // Узбекский
+      "tg", // Таджикский
+      "tk", // Туркменский
+      "az", // Азербайджанский
+      "hy", // Армянский
+      "ro", // Румынский (Молдова)
+    ];
+
+    const browserLang = navigator.language.slice(0, 2);
+    console.log(navigator.language);
+
+    if (cyrillicLanguages.includes(browserLang)) {
+      langToSelect = "ru";
+    } else if (browserLang === "fr") {
+      langToSelect = "fr";
+    } else if (browserLang === "es") {
+      langToSelect = "es";
+    } else if (browserLang === "de") {
+      langToSelect = "de";
+    } else if (browserLang === "zh") {
+      langToSelect = "zh";
+    } else {
+      langToSelect = "en";
+    }
+
+    // Save to localStorage
+    localStorage.setItem(STORAGE_KEY, langToSelect);
+  }
+
+  // Set active lang
   const matchedOption = Array.from(options).find((option) => {
     const img = option.querySelector("img");
     return img && img.getAttribute("alt") === savedLang;
   });
-
   if (matchedOption) {
     triggerContent.innerHTML = matchedOption.innerHTML;
     options.forEach((opt) => opt.classList.remove("active"));
     matchedOption.classList.add("active");
   }
 
+  // Remove loading
   document.body.classList.remove("hidden-before-init");
 });
